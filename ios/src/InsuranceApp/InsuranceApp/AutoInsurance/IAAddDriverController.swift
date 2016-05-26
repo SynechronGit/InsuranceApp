ae6fe914872921a3336c209ab2a75409561fa602
Rupendra <rupendra@example.com>
2016-05-26 16:09:29 +0530

    Add driver implementation started.

---
+++ ios/src/InsuranceApp/InsuranceApp/AutoInsurance/IAAddDriverController.swift
@@ -0,0 +1,17 @@
+//
+//  IAAddDriverController.swift
+//  InsuranceApp
+//
+
+import UIKit
+
+
+/**
+ * Controller for Add Driver screen.
+ */
+class IAAddDriverController: IABaseController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
