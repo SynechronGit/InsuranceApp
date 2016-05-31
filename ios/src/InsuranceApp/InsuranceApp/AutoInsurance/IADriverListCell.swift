7808dc2dfc4b6dce995edbdeda492e47e96e16c0
nikhil bahalkar <nikhilbahalkar@nikhils-MacBook-Pro.local>
2016-05-31 16:26:21 +0530

    *Auto Insurance Details Screen UI updated *Drivers list and Vehicle list controller implemented with dummy data

---
+++ ios/src/InsuranceApp/InsuranceApp/AutoInsurance/IADriverListCell.swift
@@ -0,0 +1,15 @@
+//
+//  IADriverListCell.swift
+//  InsuranceApp
+//
+//  Created by nikhil bahalkar on 31/05/16.
+//  Copyright © 2016 com. All rights reserved.
+//
+
+import UIKit
+class IADriverListCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var driverNameLabel: UILabel!
+    @IBOutlet weak var driverImage: UIImageView!
+}
---
+++ ios/src/InsuranceApp/InsuranceApp/AutoInsurance/IAVehicleListCell.swift
@@ -0,0 +1,19 @@
+//
+//  IAVehicleListCell.swift
+//  InsuranceApp
+//
+//  Created by nikhil bahalkar on 31/05/16.
+//  Copyright © 2016 com. All rights reserved.
+//
+
+import Foundation
+
+
+
+import UIKit
+class IAVehicleListCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var modelNoLabel: UILabel!
+    @IBOutlet weak var vehicleImageView: UIImageView!
+}
