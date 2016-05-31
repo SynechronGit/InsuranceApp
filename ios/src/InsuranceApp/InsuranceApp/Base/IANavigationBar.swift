9f7c94782224bf35dc8c3f8e0881729f841bce74
Rupendra <rupendra@example.com>
2016-05-31 15:24:09 +0530

    1) Dashboard images added. 2) Navigation bar color set.

--- ios/src/InsuranceApp/InsuranceApp/Base/IANavigationBar.swift
+++ ios/src/InsuranceApp/InsuranceApp/Base/IANavigationBar.swift
@@ -9,4 +9,14 @@
 import UIKit
 
 class IANavigationBar: UINavigationBar {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.barStyle  = UIBarStyle.BlackOpaque
+        self.backgroundColor = UIColor(red: 11.0/255.0, green: 9.0/255.0, blue: 16.0/255.0, alpha: 1.0)
+        self.tintColor = UIColor.whiteColor()
+        self.barTintColor = UIColor(red: 11.0/255.0, green: 9.0/255.0, blue: 16.0/255.0, alpha: 1.0)
+        self.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
+        self.shadowImage = UIImage()
+    }
 }
