4ad4121167d1aab70e5775315cab83bfa2942f82
Rupendra <rupendra@example.com>
2016-06-02 20:02:10 +0530

    Driver details screen implemented.

--- ios/src/InsuranceApp/InsuranceApp/AutoInsurance/DriverInsurance/IADriverDetailsController.swift
+++ ios/src/InsuranceApp/InsuranceApp/AutoInsurance/DriverInsurance/IADriverDetailsController.swift
@@ -16,6 +16,12 @@ class IADriverDetailsController: IABaseController {
     @IBOutlet weak var drivingExperienceLabel: UILabel!
     @IBOutlet weak var employeeTypeLabel: UILabel!
     
+    @IBOutlet weak var trafficViolationTableView: UITableView!
+    var trafficViolationArray: Array<IATrafficViolation>!
+    
+    @IBOutlet weak var scoreMyDriveAppContainerView: UIView!
+    @IBOutlet weak var registerSafteyDeviceContainerView: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +31,90 @@ class IADriverDetailsController: IABaseController {
         
         self.drivingLicenseNumberContainerView.layer.cornerRadius = IAConstants.dashboardSubviewCornerRadius
         self.drivingLicenseNumberContainerView.layer.masksToBounds = true
+        
+        self.scoreMyDriveAppContainerView.layer.cornerRadius = IAConstants.dashboardSubviewCornerRadius
+        self.scoreMyDriveAppContainerView.layer.masksToBounds = true
+        
+        //self.registerSafteyDeviceContainerView.layer.cornerRadius = IAConstants.dashboardSubviewCornerRadius
+        //self.registerSafteyDeviceContainerView.layer.masksToBounds = true
+        
+        self.reloadAllData()
+    }
+    
+    
+    func reloadAllData() {
+        self.trafficViolationArray = Array<IATrafficViolation>()
+        
+        var aTrafficViolation = IATrafficViolation()
+        aTrafficViolation.date = "04 - 08 - 2010"
+        aTrafficViolation.type = "Accelerating"
+        aTrafficViolation.location = "15/S Street"
+        self.trafficViolationArray.append(aTrafficViolation)
+        
+        aTrafficViolation = IATrafficViolation()
+        aTrafficViolation.date = "04 - 08 - 2010"
+        aTrafficViolation.type = "Accelerating"
+        aTrafficViolation.location = "15/S Street"
+        self.trafficViolationArray.append(aTrafficViolation)
+        
+        aTrafficViolation = IATrafficViolation()
+        aTrafficViolation.date = "04 - 08 - 2010"
+        aTrafficViolation.type = "Stop Signs"
+        aTrafficViolation.location = "15/S Street"
+        self.trafficViolationArray.append(aTrafficViolation)
+        
+        aTrafficViolation = IATrafficViolation()
+        aTrafficViolation.date = "04 - 08 - 2010"
+        aTrafficViolation.type = "Accelerating"
+        aTrafficViolation.location = "15/S Street"
+        self.trafficViolationArray.append(aTrafficViolation)
+        
+        aTrafficViolation = IATrafficViolation()
+        aTrafficViolation.date = "04 - 08 - 2010"
+        aTrafficViolation.type = "Cornering"
+        aTrafficViolation.location = "15/S Street"
+        self.trafficViolationArray.append(aTrafficViolation)
+    }
+    
+    
+    func reloadAllView() {
+        self.trafficViolationTableView.reloadData()
+    }
+    
+    
+    // MARK: - UITableView Methods
+    
+    /**
+     * Method that will calculate and return number of rows in given section of table.
+     * @return Int. Number of rows in given section
+     */
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var aReturnVal :Int = 0
+        
+        if self.trafficViolationArray != nil {
+            aReturnVal = self.trafficViolationArray.count
+        }
+        
+        return aReturnVal
+    }
+    
+    
+    /**
+     * Method that will init, set and return the cell view.
+     * @return UITableViewCell. View of the cell in given table view.
+     */
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let aReturnVal:IATrafficViolationTableCellView = tableView.dequeueReusableCellWithIdentifier("IATrafficViolationTableCellViewID") as! IATrafficViolationTableCellView
+        
+        if self.trafficViolationArray != nil {
+            let aTrafficViolation = self.trafficViolationArray[indexPath.row]
+            
+            aReturnVal.dateLabel?.text = aTrafficViolation.date
+            aReturnVal.violationTypeLabel?.text = aTrafficViolation.type
+            aReturnVal.locationLabel?.text = aTrafficViolation.location
+        }
+        
+        return aReturnVal
     }
     
 }
---
+++ ios/src/InsuranceApp/InsuranceApp/Reusables/Models/IATrafficViolation.swift
@@ -0,0 +1,15 @@
+//
+//  IATrafficViolation.swift
+//  InsuranceApp
+//
+//  Created by rupendra on 6/2/16.
+//  Copyright © 2016 com. All rights reserved.
+//
+
+import UIKit
+
+class IATrafficViolation: NSObject {
+    var date :String!
+    var type :String!
+    var location :String!
+}
