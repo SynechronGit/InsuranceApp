12cfab062afe043f7cc63a1140777b86befaca39
nikhil bahalkar <nikhilbahalkar@nikhils-MacBook-Pro.local>
2016-06-09 15:47:13 +0530

    1) Score My Drive Started 2) My claims on dashboard implemented (model and query) 3) controller and scene added for score my drive and safety device

---
+++ ios/src/InsuranceApp/InsuranceApp/AutoInsurance/DriverInsurance/IARegisterSafetyDeviceController.swift
@@ -0,0 +1,13 @@
+//
+//  IARegisterSafetyDeviceController.swift
+//  InsuranceApp
+//
+//  Created by nikhil bahalkar on 09/06/16.
+//  Copyright © 2016 com. All rights reserved.
+//
+
+import UIKit
+
+class IARegisterSafetyDeviceController: UIViewController {
+
+}
---
+++ ios/src/InsuranceApp/InsuranceApp/AutoInsurance/DriverInsurance/IAScoreMyDriveController.swift
@@ -0,0 +1,13 @@
+//
+//  IAScoreMyDriveController.swift
+//  InsuranceApp
+//
+//  Created by nikhil bahalkar on 09/06/16.
+//  Copyright © 2016 com. All rights reserved.
+//
+
+import UIKit
+
+class IAScoreMyDriveController: UIViewController {
+
+}
